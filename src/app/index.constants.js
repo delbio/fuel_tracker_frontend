be34cb44e5e32b15480d086e46551c50a0315463
Fabio Del Bene <delbio87@gmail.com>
2016-04-16 14:25:10 +0200

    yo gulp-angular installation

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('fuelTrackerFrontend')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
